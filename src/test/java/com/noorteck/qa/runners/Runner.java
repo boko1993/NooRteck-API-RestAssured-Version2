f7450854c042bc26ff505da955e707ad39d7ac59
bokan <boko1@DESKTOP-KJE9GV6.myfiosgateway.com>
2023-01-21 12:18:52 -0500

    editted again

--- src/test/java/com/noorteck/qa/runners/Runner.java
+++ src/test/java/com/noorteck/qa/runners/Runner.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 
 @CucumberOptions(
-					features = "src/test/resources/hr/US305-GetSpeJobByID.feature",
+					features = "src/test/resources/hr",
 					glue = "com.noorteck.qa.steps",
 				
 					
